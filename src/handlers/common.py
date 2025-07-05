688ce08a38c6a4e851808fa321e7709a2f108e96
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-05 22:01:37 +0300

    Add developer check & refactor CommonHandler in common.py

--- src/handlers/common.py
+++ src/handlers/common.py
@@ -5,62 +5,72 @@ from aiogram.enums import ParseMode
 
 from .base import BaseHandler
 from src.keyboards.reply import HubMenu, HubTeacher
-from src.keyboards.inline import HubAdmin
+from src.keyboards.inline import HubAdmin, DeveloperHub
 from .register import RegisterHandler
 from settings.admins import Admins
+from settings.developers import Developers
 
 
 class CommonHandler(BaseHandler):
     def register_handler(self) -> None:
         self.router.message.register(self.start_cmd, CommandStart())
 
-
     async def start_cmd(self, message: Message, state: FSMContext) -> None:
         """Обробник команди start"""
         user_id = message.from_user.id
 
-        if user_id in Admins().ADMINS:
+        admins = Admins().ADMINS
+        developers = Developers().DEVELOPERS
+
+        if user_id in developers:
             await message.answer(
-                "<b>👋 Вітаємо в чат-боті Березанського ліцею №3!</b>\n\n"
-                "Ви зареєстровані як <b>адміністратор ЗЗСО</b>. Тепер ви можете:\n\n"
-                "• надсилати зміни в розкладі;\n"
-                "• надсилати оголошення учням та вчителям;\n"
-                "• заплановувати наради.\n\n"
-                "📽 Щоб ознайомитися з роботою бота, будь ласка, перегляньте відео нижче.",
-                parse_mode=ParseMode.HTML,
-                reply_markup=HubAdmin().get_keyboard()
+                "The developer mode has been activated. 🚀",
+                reply_markup=DeveloperHub().get_keyboard()
             )
-            return
 
         if self.db.register.checker(user_id):
-            """Якщо користувач зареєстрований"""
-            if self.db.register.get_type(user_id) == 'student':
-                form = self.db.register.get_class(user_id)
-                prompt = (f"👋🏻 Вітаємо в чат-боті <b>Березанського ліцею №3</b>\n\n"
-                          f"Ви зареєстровані як учень <b>{form}</b> класу.\n\n"
-                          f"Якщо хочете змінити дані, використайте команду /register")
-                rm = HubMenu().get_keyboard()
-            else:
-                teacher_name = self.db.register.get_teacher_name(user_id)
-                prompt = (f"👋🏻 Вітаємо в чат-боті <b>Березанського ліцею №3</b>\n\n"
-                          f"Ви зареєстровані як вчитель <b>{teacher_name}</b>\n\n"
-                          f"Якщо хочете змінити дані, використайте команду /register")
-                rm = HubTeacher().get_keyboard()
+            # якщо користувач зареєстрований
+            user_type = self.db.register.get_type(user_id)
 
-            await message.answer(
-                prompt,
-                reply_markup=rm,
-                parse_mode=ParseMode.HTML
-            )
+            match user_type:
+                case 'student':
+                    form = self.db.register.get_class(user_id)
+                    prompt = (
+                        f"👋🏻 Вітаємо в чат-боті <b>Березанського ліцею №3</b>\n\n"
+                        f"Ви зареєстровані як учень <b>{form}</b> класу.\n\n"
+                        f"Якщо хочете змінити дані, використайте команду /register"
+                    )
+                    rm = HubMenu().get_keyboard()
+                case "teacher":
+                    teacher_name = self.db.register.get_teacher_name(user_id)
+                    prompt = (
+                        f"👋🏻 Вітаємо в чат-боті <b>Березанського ліцею №3</b>\n\n"
+                        f"Ви зареєстровані як вчитель <b>{teacher_name}</b>\n\n"
+                        f"Якщо хочете змінити дані, використайте команду /register"
+                    )
+                    rm = HubTeacher().get_keyboard()
+                case _:
+                    prompt = (
+                        "⚠️ Помилка: тип вашого акаунту не розпізнано.\n"
+                        "Зверніться до адміністратора або пройдіть реєстрацію заново (/register)"
+                    )
+                    rm = None
+
+            await message.answer(prompt, reply_markup=rm, parse_mode=ParseMode.HTML)
+
+            if user_id in admins:
+                await message.answer(
+                    "👑 Панель андміністратора ввікмнена.",
+                    reply_markup=HubAdmin().get_keyboard()
+                )
 
         else:
-            """Відправляємо користувача на реєстрацію"""
+            # якщо користувач не зареєстрований
             await message.answer(
                 "👋🏻 Вітаємо в чат-боті <b>Березанського ліцею №3</b>\n\n"
                 "Будь-ласка, зареєструйтеся, щоб скористатись всіма можливостями бота\n\n"
-                "📢 Бот неофіційний. Його зробив дефолтний учень, а не адміністрація — тому якщо щось глючить, "
-                "не пишіть завучу xd. (овнер @noinsts)\n\n"
-                "ℹ️ Розклад був отриманий з оф. сайту школи (bnvk.pp.ua)",
+                "⚙️ Telegram-канал бота: поки що відсутній (скоро буде)",
+                # TODO: додати посилання на тгк бота, коли буде
                 parse_mode=ParseMode.HTML
             )
             await RegisterHandler().start_register(message, state)
