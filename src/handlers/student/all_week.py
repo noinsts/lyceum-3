6cd73e517ab93017d50c6a9f1a53c7eaaf28c885
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-16 21:44:50 +0300

    feat: update AllWeek handler with teacher name parsing and docstring
    - Added parsing of teacher names using vocative_teacher_names.json
    - Included docstring to AllWeek handler
    - Added vocative_teacher_names.json file to project

--- src/handlers/student/all_week.py
+++ src/handlers/student/all_week.py
@@ -5,31 +5,54 @@ from aiogram.types import Message
 from aiogram.enums import ParseMode
 
 from ..base import BaseHandler
+from src.utils import JSONLoader
+
 
 class AllWeekHandler(BaseHandler):
-     def register_handler(self):
-          self.router.message.register(self.all_week, F.text == '📝 Розклад на весь тиждень')
+    def register_handler(self):
+        self.router.message.register(self.all_week, F.text == '📝 Розклад на весь тиждень')
 
-     async def all_week(self, message: Message) -> None:
+    async def all_week(self, message: Message) -> None:
+        """
+        Обробка відправлення студенту розкладу на весь тиждень
+        """
+
         user_class = self.db.register.get_class(message.from_user.id)
         results = self.sheet.student.get_lessons(user_class)
 
+        # сортуємо по днях
         lessons_by_day = defaultdict(list)
-
-        # додаємо дані в lessons_by_day
         for day, number, subject, teacher in results:
             lessons_by_day[day].append((number, subject, teacher))
 
+        # початок повідомлення
         prompt = f"<b>Список уроків {user_class} класу</b>\n"
 
-        # зчитуємо та виводимо дані
+        # завантажуємо файл з орудними відмінками
+        vocative_names = JSONLoader("settings/vocative_teacher_name.json")
+
         for day, lessons in lessons_by_day.items():
             prompt += f"\n<b>{day.capitalize()}</b>\n"
             for number, subject, teacher in lessons:
+                # парсимо дані відповідно до тижня
                 subject, teacher = self.wf.student(subject, teacher)
-                if subject:
-                    prompt += f"<b>{number}</b>: {subject} з {teacher.replace(",", " та")}\n"
 
+                if not subject:
+                    continue
+
+                # масив з імена вчителів
+                teacher_names = []
+
+                # TODO: винести парсер в окремий файл
+                # парсомо імена вчителів за орудним відмінком
+                for t in map(str.strip, teacher.split(',')):
+                    voc_name = vocative_names.get(t)
+                    teacher_names.append(voc_name if voc_name else t)
+
+                teacher_string = " та ".join(teacher_names)
+                prompt += f"<b>{number}</b>: {subject} з {teacher_string}\n"
+
+        # виводимо дані
         await message.answer(
             prompt,
             parse_mode=ParseMode.HTML
