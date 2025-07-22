2258bc8e503878853145b002dfcb45244904f5a8
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-22 12:36:36 +0300

    refactor: remove src/utils/state to split into separate files

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,4 @@
 from .logger import setup_logger
-from .states import RegisterStates
 from .classes_array import classes
 from .json import JSONLoader
 from .week_state import WeekFormat
@@ -8,7 +7,6 @@ from .parse_hub_keyboard import parse_hub_keyboard
 
 __all__ = [
     "setup_logger",
-    "RegisterStates",
     "classes",
     "JSONLoader",
     "WeekFormat",
