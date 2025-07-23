873b5af0cdc0886e865b3c86c6f6ac50e8d5f0c8
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-24 00:31:57 +0300

    fix(db): change primary key type from Integer to BigInteger for olymp and teacher verification models

--- src/db/models/olymp.py
+++ src/db/models/olymp.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Date
+from sqlalchemy import String, BigInteger, Date
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import BaseModel
@@ -7,7 +7,7 @@ from .base import BaseModel
 class OlympModel(BaseModel):
     __tablename__ = 'olymp'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     form: Mapped[str] = mapped_column(String(5), nullable=False)
     student_name: Mapped[str] = mapped_column(String(50), nullable=False)
     teacher_name: Mapped[str] = mapped_column(String(50), nullable=False)
--- src/db/models/verification.py
+++ src/db/models/verification.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Boolean
+from sqlalchemy import String, BigInteger, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import BaseModel
@@ -7,6 +7,6 @@ from .base import BaseModel
 class TeacherVerificationModel(BaseModel):
     __tablename__ = 'teacher_verification'
 
-    user_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     teacher_name: Mapped[str] = mapped_column(String, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
