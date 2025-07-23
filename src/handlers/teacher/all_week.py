5c772a308adbc68a4a040441b05fa7821caab14d
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-23 16:22:34 +0300

    refactor(handler): cleanup all week handler (teacher), integrate sheet logic, follow PEP8

--- src/handlers/teacher/all_week.py
+++ src/handlers/teacher/all_week.py
@@ -1,5 +1,3 @@
-# FIXME: додати логіку з нової sheet
-
 from collections import defaultdict
 
 from aiogram import F
@@ -9,15 +7,23 @@ from aiogram.enums import ParseMode
 from ..base import BaseHandler
 from src.db.connector import DBConnector
 
+WEEKDAYS = ('ПОНЕДІЛОК', 'ВІВТОРОК', 'СЕРЕДА', 'ЧЕТВЕР', "П'ЯТНИЦЯ")
+
 
 class AllWeekHandler(BaseHandler):
     def register_handler(self):
-        pass
-        # self.router.message.register(self.all_week, F.text == '📝 Тижневий розклад')
+        self.router.message.register(self.all_week, F.text == '📝 Тижневий розклад')
 
     async def all_week(self, message: Message, db: DBConnector) -> None:
-        tn = await db.register.get_teacher_name(message.from_user.id)
-        results = self.db.teacher.get_all_week(tn)
+        teacher_name = await db.register.get_teacher_name(message.from_user.id)
+        results = self.sheet.teacher.get_lessons(teacher_name)
+
+        if not results:
+            await message.answer(
+                "Схоже, у вас цього тижня немає жодного уроку. "
+                "Вітаю, ви або в відпустці, або дуже щасливий викладач 😎"
+            )
+            return
 
         by_day = defaultdict(list)
 
@@ -26,9 +32,13 @@ class AllWeekHandler(BaseHandler):
 
         prompt = "<b>Розклад на тиждень</b>\n\n"
 
-        for day, lessons in by_day.items():
+        for day in WEEKDAYS:
+            if day not in by_day:
+                continue
+
             prompt += f"\n<b>{day.capitalize()}</b>\n"
-            for number, subject, form in lessons:
+
+            for number, subject, form in sorted(by_day[day]):
                 prompt += f"<b>{number}</b>: {subject} з {form}\n"
 
         await message.answer(
