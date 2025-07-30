6a504546f1d2d84e9086762378648e4bc60255bb
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-30 12:10:55 +0300

    remove(fsm): student_olymp.py

--- src/states/__init__.py
+++ src/states/__init__.py
@@ -1,13 +1,11 @@
 from .register import  RegisterStates
 from .teacher_type import TeacherTypesStates
-from .student_olymp import StudentOlympsStates
 from .create_olymp import CreateOlympStates
 from .add_access import DevAddAccess
 
 __all__ = [
     "RegisterStates",
     "TeacherTypesStates",
-    "StudentOlympsStates",
     "CreateOlympStates",
     "DevAddAccess"
 ]
--- src/states/student_olymp.py
+++
@@ -1,4 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class StudentOlympsStates(StatesGroup):
-    waiting_for_name = State()
