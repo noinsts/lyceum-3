aff97cad40485280611f190a436b069bf231597c
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-31 12:54:40 +0300

    feat(filters): add FormsListCallback filter

--- src/filters/callbacks/__init__.py
+++ src/filters/callbacks/__init__.py
@@ -1,7 +1,9 @@
 from .teacher_type import TeacherCategoryCallback
 from .teachers_list import TeacherListCallback
+from .forms_list import FormsListCallback
 
 __all__ = [
     "TeacherCategoryCallback",
-    "TeacherListCallback"
+    "TeacherListCallback",
+    "FormsListCallback"
 ]
