853e765f7843d0bf1c3a06c910f214416c95e0f0
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-09 16:46:05 +0300

    feat(dev-panel): add ascii art into AccessHubHandler

--- src/handlers/developer/access/hub.py
+++ src/handlers/developer/access/hub.py
@@ -5,19 +5,34 @@ from ...base import BaseHandler
 from src.keyboards.inline import DeveloperAccessHub
 
 
+class Triggers:
+    HANDLER = "dev_access_hub"
+
+
+class Messages:
+    ASCII = r"""
+    
+    (dev)
+    
+        _                   
+       /_\  __ __ ___ ______
+      / _ \/ _/ _/ -_|_-<_-<
+     /_/ \_\__\__\___/__/__/
+                            
+    """
+
+
 class AccessHubHandler(BaseHandler):
     def register_handler(self) -> None:
         self.router.callback_query.register(
             self.handler,
-            F.data == 'dev_access_hub'
+            F.data == Triggers.HANDLER
         )
 
     @staticmethod
     async def handler(callback: CallbackQuery) -> None:
-        # заглушка
         await callback.answer()
-
         await callback.message.answer(
-            "Select next action",
+            Messages.ASCII,
             reply_markup=DeveloperAccessHub().get_keyboard()
         )
