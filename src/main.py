f778692d732475cbabf0bef5ec7d9ec7a4cd3a37
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-09 14:48:19 +0300

    feat: add FSM state caching using RedisStorage, remove FlaskServer from main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,17 @@
-"""
-O magna Salnikova, munda peccata nostra, et veni ad nos, clamor noster auditur.
-"""
-
-"""
-йобаний насос, я ніхуя в цьому коду не понімаю
-хто це блять писав
-"""
-
 import asyncio
 import os
-import socket
-from threading import Thread
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from dotenv import load_dotenv
-from flask import Flask
-from pyngrok import ngrok
 
 from src.bot_instance import set_bot
 from src.handlers import get_all_router
 from src.utils import setup_logger
 from src.db.db import create_db
 from src.middlewares import DBMiddleware, LoggingMiddleware
+from src.sheets.connector import get_sheet, get_redis
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -33,57 +22,55 @@ class LyceumBot:
         self.bot = Bot(token=TOKEN)
         set_bot(self.bot)
 
-        self.storage = MemoryStorage()
-        self.dp = Dispatcher(storage=self.storage)
+        self.storage = None
+        self.dp = None
         self.log = setup_logger()
-
-        # підключення database middleware
-        self.dp.message.middleware(DBMiddleware())
-        self.dp.callback_query.middleware(DBMiddleware())
-
-        # підключення logging middleware
-        self.dp.message.middleware(LoggingMiddleware())
-        self.dp.callback_query.middleware(LoggingMiddleware())
-
-        self.dp.include_router(get_all_router())
 
     async def run(self):
         try:
+            redis = await get_redis()
+
+            if redis is None:
+                self.log.warning("Redis недоступний! Використовую MemoryStorage для FSM")
+                self.storage = MemoryStorage()
+            else:
+                self.log.info("Redis підключено успішно!")
+                self.storage = RedisStorage(redis=redis)
+
+            self.dp = Dispatcher(storage=self.storage)
+
+            sheet = await get_sheet()
+            self.log.info(f"Google Sheets підключено! {str(sheet)}")
+
+            # підключення database middleware
+            db_middleware = DBMiddleware()
+            self.dp.message.middleware(db_middleware)
+            self.dp.callback_query.middleware(db_middleware)
+
+            # підключення logging middleware
+            logging_middleware = LoggingMiddleware()
+            self.dp.message.middleware(logging_middleware)
+            self.dp.callback_query.middleware(logging_middleware)
+
+            self.dp.include_router(get_all_router())
+
             await create_db()
+            self.log.info("БД підключено")
+
             self.log.info('Бот запущено!')
             await self.dp.start_polling(self.bot)
+
+        except Exception as e:
+            self.log.error(f"Помилка запуску бота: {e}")
+            raise
+
         finally:
+            sheet = await get_sheet()
+            await sheet.close()
+
             await self.bot.session.close()
 
 
-class FlaskServer:
-    def __init__(self, port: int = 8080):
-        self.app = Flask(__name__)
-        self.port = port
-        self.log = setup_logger()
-        self.setup_routes()
-
-    def setup_routes(self) -> None:
-        @self.app.route('/')
-        def home():
-            return "Бот живий", 200
-
-    def run(self) -> None:
-        self.app.run(host="0.0.0.0", port=self.port)
-
-    def run_in_background(self) -> None:
-        thread = Thread(target=self.run)
-        thread.daemon = True
-        thread.start()
-
-        public_url = ngrok.connect(self.port, "http")
-
-        self.log.info(f"{public_url}")
-
-
 if __name__ == "__main__":
-    server = FlaskServer(port=8080)
-    server.run_in_background()
-
     bot = LyceumBot()
     asyncio.run(bot.run())
