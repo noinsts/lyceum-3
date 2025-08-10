1291b7de82a0f3bde80725f8a1b0207fbb0dd12c
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-10 14:41:17 +0300

    refactor(decorators): update next_state to skip FSM change on ValidationError

--- src/decorators/fsm.py
+++ src/decorators/fsm.py
@@ -3,6 +3,8 @@ from functools import wraps
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
+from src.exceptions import ValidationError
+
 
 def next_state(state_to_set):
     """
@@ -11,7 +13,10 @@ def next_state(state_to_set):
     def decorator(handler_func):
         @wraps(handler_func)
         async def wrapper(self, message: Message, state: FSMContext, *args, **kwargs):
-            await handler_func(self, message, state, *args, **kwargs)
-            await state.set_state(state_to_set)
+            try:
+                await handler_func(self, message, state, *args, **kwargs)
+                await state.set_state(state_to_set)
+            except ValidationError:
+                pass
         return wrapper
     return decorator
