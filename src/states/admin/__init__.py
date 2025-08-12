357673753c514b818475a410f0198f9328e9ba92
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-12 12:27:30 +0300

    feat(fsm): add TeacherFormStates state group (admin)

--- src/states/admin/__init__.py
+++ src/states/admin/__init__.py
@@ -1,7 +1,9 @@
 from .schedule.student import StudentSchedule
 from .schedule.teacher import TeacherSchedule
+from .forms.teacher_form import TeacherFormStates
 
 __all__ = [
     "StudentSchedule",
-    "TeacherSchedule"
+    "TeacherSchedule",
+    "TeacherFormStates"
 ]
