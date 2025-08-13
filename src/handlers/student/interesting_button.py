edc5d0eb2010645071134dc338b2ec8bad2b77bd
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-13 19:22:15 +0300

    ui: updated responses for students

--- src/handlers/student/interesting_button.py
+++ src/handlers/student/interesting_button.py
@@ -14,6 +14,11 @@ class InterestingButtonHandler(BaseHandler):
 
     @staticmethod
     async def handler(message: Message) -> None:
+        interesting_button = (
+            "<b>🌎 Цікава кнопка</b> - це кнопка, що оновлюється щодня і пропонує цікаву та корисну інформацію. "
+            "Тут завжди можна знайти щось нове і несподіване, що зробить твій день трішки яскравішим."
+        )
+
         prompt = (
             "<b>Мало хто знає, але Остапенко Михайло за це літо був три рази ведучим</b>\n\n"
             "🔹 на випускному 9-х класів\n"
@@ -22,4 +27,5 @@ class InterestingButtonHandler(BaseHandler):
             "<i>7/27/2025 — Андрій (noinsts)</i>"
         )
 
+        await message.answer(interesting_button, parse_mode=ParseMode.HTML)
         await message.answer(prompt, parse_mode=ParseMode.HTML)
