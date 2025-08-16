e329e2d9c40ef02a5c2ee047a0b3100ac118f05f
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-16 20:43:21 +0300

    chore: remove src/handlers/docs_callbacks directory

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -7,7 +7,6 @@ from .teacher import get_teacher_router
 from .admin import get_admin_router
 from .common import get_common_router
 from .developer import get_dev_routers
-from .docs_callbacks import get_docs_routers
 
 
 def get_all_router() -> Router:
@@ -24,8 +23,6 @@ def get_all_router() -> Router:
     main_router.include_router(get_admin_router())
 
     main_router.include_router(get_dev_routers())
-
-    main_router.include_router(get_docs_routers())
 
     main_router.include_router(get_common_router())
 
