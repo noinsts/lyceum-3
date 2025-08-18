d5d375842cbb2b688a1d7d037702950357ea7963
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-18 21:03:23 +0300

    feat(db): add description column for CardModel

--- src/db/models/card.py
+++ src/db/models/card.py
@@ -11,6 +11,7 @@ class CardModel(BaseModel):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
     sticker_id: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     rarity: Mapped[RarityCardsEnum] = mapped_column(
         PgEnum(RarityCardsEnum, name="rarity_card_enum"), nullable=False
