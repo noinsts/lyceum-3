3e139343dd0564b51fab52d16a34bd7c34339d1f
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-18 15:57:58 +0300

    feat(enums): add RarityCardsEnum

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -3,11 +3,13 @@ from .db_utype import DBUserType
 from .teacher_type import TeacherTypeEnum
 from .olympiad_stages import OlympStage
 from .depth_study import DepthSubjectEnum
+from .rarity_cards import RarityCardsEnum
 
 __all__ = [
     "UserType",
     "DBUserType",
     "TeacherTypeEnum",
     "OlympStage",
-    "DepthSubjectEnum"
+    "DepthSubjectEnum",
+    "RarityCardsEnum"
 ]
