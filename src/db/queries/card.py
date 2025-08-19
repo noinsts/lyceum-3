a63dd4653aa32e0e220ff3d77ef13a42f69adec0
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-19 13:05:45 +0300

    feat(db): add new_card method in CardQueries

--- src/db/queries/card.py
+++ src/db/queries/card.py
@@ -1,6 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+
+from ..models import CardModel
+from ..schemas import CardSchema
 
 
 class CardQueries:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def new_card(self, data: CardSchema) -> None:
+        try:
+            card = CardModel(**data.model_dump())
+            self.session.add(card)
+            await self.session.commit()
+        except SQLAlchemyError as e:
+            await self.session.rollback()
+            raise e
