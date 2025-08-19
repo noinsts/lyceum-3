3499d43c30fb95909643408309c7fb4372b9890e
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-19 13:04:50 +0300

    feat(fsm): add AddStates state group for developer/collections

--- src/states/developer/__init__.py
+++ src/states/developer/__init__.py
@@ -2,8 +2,11 @@ from .access.block import AccessBlock
 from .access.unblock import AccessUnblock
 from .access.status import AccessStatus
 
+from .collections.add import AddStates
+
 __all__ = [
     "AccessBlock",
     "AccessUnblock",
-    "AccessStatus"
+    "AccessStatus",
+    "AddStates"
 ]
