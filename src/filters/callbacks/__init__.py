6cd575be37fd9defca81531f6232a233b2795f12
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-19 13:05:07 +0300

    feat(filters): add CardRarityCallback

--- src/filters/callbacks/__init__.py
+++ src/filters/callbacks/__init__.py
@@ -5,6 +5,7 @@ from .developer_block import DeveloperSearchEnum, DeveloperSearchCallback
 from .depth_subject import DepthSubjectCallback
 from .broadcast_type import BroadcastTypeCallback, BroadcastTypeEnum
 from .teacher_verify import TeacherVerifyCallback, TeacherVerifyEnum
+from .card_rarity import CardRarityCallback
 
 __all__ = [
     "TeacherCategoryCallback",
@@ -16,5 +17,6 @@ __all__ = [
     "BroadcastTypeCallback",
     "BroadcastTypeEnum",
     "TeacherVerifyCallback",
-    "TeacherVerifyEnum"
+    "TeacherVerifyEnum",
+    "CardRarityCallback"
 ]
