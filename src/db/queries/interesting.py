f73e27c9636dd3ca81345dc7ed6573871a31e3d9
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-21 16:15:12 +0300

    feat(db): add get_by_date method in InterestingQueries

--- src/db/queries/interesting.py
+++ src/db/queries/interesting.py
@@ -10,3 +10,9 @@ from ..models import InterestingModel
 class InterestingQueries:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def get_by_date(self, date: datetime.date) -> str:
+        """Повертає промпт за датою"""
+        query = select(InterestingModel.prompt).where(InterestingModel.date == date)
+        result = await self.session.execute(query)
+        return result.scalar()
