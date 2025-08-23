76cf7bd0898dd7ed658d10791156f0a2878f4ab4
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-23 16:43:10 +0300

    feat(db): add get_day method into ShortenedQueries

--- src/db/queries/shortened.py
+++ src/db/queries/shortened.py
@@ -10,3 +10,9 @@ from ..models import ShortenedDayModel
 class ShortenedQueries:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def get_day(self, date: datetime.date) -> Optional[ShortenedDayModel]:
+        """Повертає інформацію про день за датою"""
+        query = select(ShortenedDayModel).where(ShortenedDayModel.date == date)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
