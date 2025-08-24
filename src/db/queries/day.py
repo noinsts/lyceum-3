7c80691a0e0bda3019faf4435fa3b25ea33f2a11
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-24 15:24:27 +0300

    feat(db): add set_day method into DayQueries

--- src/db/queries/day.py
+++ src/db/queries/day.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 
 from ..models import DayModel
+from ..schemas import DaySchema
 
 
 class DayQueries:
@@ -17,3 +18,13 @@ class DayQueries:
         query = select(DayModel).where(DayModel.date == date)
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
+
+    async def set_day(self, day: DaySchema) -> None:
+        """Встановлює статус для конкретного дня"""
+        try:
+            obj = DayModel(**day.model_dump())
+            self.session.add(obj)
+            await self.session.commit()
+        except SQLAlchemyError as e:
+            await self.session.rollback()
+            raise e
