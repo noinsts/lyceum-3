b4ac51e6016277f418df45915a7142976322d68a
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-25 11:31:07 +0300

    refactor(keyboards): edit callback data of "Create announcement" button in HubAdmin inline keyboard

--- src/keyboards/inline.py
+++ src/keyboards/inline.py
@@ -33,7 +33,7 @@ class BackToAdminHub(BaseKeyboard):
 class HubAdmin(BaseKeyboard):
     def get_keyboard(self) -> InlineKeyboardMarkup:
         kb = [
-            [InlineKeyboardButton(text='📢 Створити оголошення', callback_data="announcement_hub")],
+            [InlineKeyboardButton(text='📢 Створити оголошення', callback_data="admin_broker")],
             [InlineKeyboardButton(text='📅 Змінити розклад', callback_data='admin_schedule_hub')],
             [InlineKeyboardButton(text='🫐 Керування класами', callback_data='admin_form_controller_hub')],
             [InlineKeyboardButton(text='⏰ Скорочення дня', callback_data="admin_shortened_hub")]
