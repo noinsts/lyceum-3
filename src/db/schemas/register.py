1e8846107c993a3552dd1f6b4fc210161e7d57f3
Andriy <114863893+noinsts@users.noreply.github.com>
2025-08-26 14:04:58 +0300

    refactor(db): change AddUserSchema.user_type annotation from DBUserType to str

--- src/db/schemas/register.py
+++ src/db/schemas/register.py
@@ -2,14 +2,12 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from src.enums import DBUserType
-
 
 class AddUserSchema(BaseModel):
     user_id: int
     full_name: Optional[str] = None
     username: Optional[str] = None
-    user_type: DBUserType
+    user_type: str
     form: Optional[str] = None
     teacher_name: Optional[str] = None
     student_name: Optional[str] = None
